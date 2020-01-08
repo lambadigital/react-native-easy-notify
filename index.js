5f1a52d94753eb23e6a62c64241922fed1f7d040
Zeki Unal <zekiunal@gmail.com>
2020-01-08 18:02:45 +0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import { NativeModules } from 'react-native';
 
-const { EasyNotify } = NativeModules;
+const { RNEasyNotify } = NativeModules.EasyNotify;
 
-export default EasyNotify;
+export default class EasyNotify {
+    static init(id, settings) {
+        console.log("EasyNotify ID:" + id);
+        console.log("EasyNotify settings:" + settings);
+    }
+
+    /* deprecated */
+    static configure() {
+        console.log("EasyNotify configure deprecated");
+    }
+}

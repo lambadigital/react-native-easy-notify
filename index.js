f724bd1e72c36f97488adbd638451684714915e3
Zeki Unal <zekiunal@gmail.com>
2020-01-08 18:36:35 +0300

    first commit

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react';
 
 import {NativeModules} from 'react-native';
 
+import OneSignal from 'react-native-onesignal';
+
 const {RNEasyNotify} = NativeModules.EasyNotify;
 
 export default class EasyNotify extends Component {
     constructor(props) {
         super(props);
         console.log('EasyNotify constructor');
+
+        OneSignal.init('91ea50e1-5db3-4e08-a501-e7d78a41ce7e', {
+            kOSSettingsKeyAutoPrompt: true,
+        });
+
+        OneSignal.setLogLevel(6, 0);
     }
 
     UNSAFE_componentWillMount() {
@@ -21,6 +29,29 @@ export default class EasyNotify extends Component {
 
     componentDidUpdate() {
         console.log('EasyNotify componentDidUpdate');
+    }
+
+    componentWillUnmount() {
+        console.log('EasyNotify componentWillUnmount');
+
+        OneSignal.removeEventListener('received', this.onReceived);
+        OneSignal.removeEventListener('opened', this.onOpened);
+        OneSignal.removeEventListener('ids', this.onIds);
+    }
+
+    async componentDidMount() {
+        console.log('EasyNotify componentDidMount');
+        OneSignal.setLocationShared(true);
+        OneSignal.inFocusDisplaying(2);
+
+        this.onReceived = this.onReceived.bind(this);
+        this.onOpened = this.onOpened.bind(this);
+        this.onIds = this.onIds.bind(this);
+
+        OneSignal.addEventListener('received', this.onReceived);
+        OneSignal.addEventListener('opened', this.onOpened);
+        OneSignal.addEventListener('ids', this.onIds);
+
     }
 
     initialize = (props) => {
@@ -37,6 +68,30 @@ export default class EasyNotify extends Component {
     /* deprecated */
     static configure() {
         console.log('EasyNotify configure deprecated');
+    }
+
+    onReceived(notification) {
+        console.log('Notification received: ', notification);
+
+        this.setState({
+            jsonDebugText: 'RECEIVED: \n' + JSON.stringify(notification, null, 2),
+        });
+    }
+
+    onOpened(openResult) {
+        console.log('Message: ', openResult.notification.payload.body);
+        console.log('Data: ', openResult.notification.payload.additionalData);
+        console.log('isActive: ', openResult.notification.isAppInFocus);
+        console.log('openResult: ', openResult);
+
+        this.setState({
+            jsonDebugText:
+                'OPENED: \n' + JSON.stringify(openResult.notification, null, 2),
+        });
+    }
+
+    onIds(device) {
+        console.log('Device info: ', device);
     }
 
     render() {

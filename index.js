b1a7530e45a025d95798a071af0d067cdff1e147
Zeki Unal <zekiunal@gmail.com>
2020-01-08 18:25:15 +0300

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-import { NativeModules } from 'react-native';
+import React, {Component} from 'react';
 
-const { RNEasyNotify } = NativeModules.EasyNotify;
+import {NativeModules} from 'react-native';
 
-export default class EasyNotify {
+const {RNEasyNotify} = NativeModules.EasyNotify;
+
+export default class EasyNotify extends Component {
+    constructor(props) {
+        super(props);
+        console.log('EasyNotify constructor');
+    }
+
+    UNSAFE_componentWillMount() {
+        console.log('EasyNotify UNSAFE_componentWillMount');
+
+    }
+
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        console.log('EasyNotify UNSAFE_componentWillReceiveProps');
+    }
+
+    componentDidUpdate() {
+        console.log('EasyNotify componentDidUpdate');
+    }
+
+    initialize = (props) => {
+        console.log('EasyNotify initialize');
+
+        console.log(props);
+    };
+
     static init(id, settings) {
-        console.log("EasyNotify ID:" + id);
-        console.log("EasyNotify settings:" + settings);
+        console.log('EasyNotify ID:' + id);
+        console.log('EasyNotify settings:' + settings);
     }
 
     /* deprecated */
     static configure() {
-        console.log("EasyNotify configure deprecated");
+        console.log('EasyNotify configure deprecated');
+    }
+
+    render() {
+        console.log('EasyNotify render');
+
+        return null;
     }
 }
